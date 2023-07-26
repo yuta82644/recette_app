b35685d439898c8b73853c044dc8028de02e6de3
yuta <calendula6724yuta@gmail.com>
2023-07-26 13:35:02 +0900

    カテゴリーテーブルのマイグレーション実行

--- db/migrate/20230726042149_create_recipe_categories.rb
+++ db/migrate/20230726042149_create_recipe_categories.rb
@@ -1,7 +1,9 @@
 class CreateRecipeCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :recipe_categories do |t|
-
+      t.references :recipe, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+      
       t.timestamps
     end
   end

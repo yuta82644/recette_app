acda5bc3c0b7df82362cc1a7e093569b950b6406
yuta <calendula6724yuta@gmail.com>
2023-07-26 17:44:33 +0900

    お気に入り一覧ページの作成完了

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,10 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
+
+  def index
+    @favorites = current_user.favorites.includes(:recipe)
+  end
   def create
     favorite = current_user.favorites.create(recipe_id: params[:recipe])
     redirect_to recipes_path, notice: "#{favorite.recipe.user.name}さんのレシピをお気に入り登録しました"

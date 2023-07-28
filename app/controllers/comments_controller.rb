f2fca09dc837b25dd185a92ae9e38131483f00f5
yuta <calendula6724yuta@gmail.com>
2023-07-28 13:46:46 +0900

    レシピの詳細ページにコメント機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,16 +7,53 @@ class CommentsController < ApplicationController
     @comment.user = current_user
 
 
-
-    # クライアント要求に応じてフォーマットを変更
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to recipe_path(@recipe) }
+        format.js {render :index}
       else
         format.html { redirect_to recipe_path(@recipe), notice: 'コメントを投稿できませんでした...' }
       end
     end
   end
+
+  def edit 
+  @recipe = Recipe.find(params[:recipe_id])
+  @comment = @recipe.comments.find(params[:id])
+  respond_to do |format|
+    flash.now[:notice] = 'コメントの編集中'
+    format.js { render :edit }
+    
+
+    
+  end
+end
+
+  def update
+  @recipe = Recipe.find(params[:recipe_id])  # 修正：@recipe を先に定義する
+  @comment = @recipe.comments.find(params[:id])
+  
+  respond_to do |format|
+    if @comment.update(comment_params)
+      flash.now[:notice] = 'コメントが編集されました'
+      format.js { render :index }
+    else
+      flash.now[:notice] = 'コメントの編集に失敗しました'
+      format.js { render :edit }
+    end
+  end
+end
+
+
+  def destroy
+  @recipe = Recipe.find(params[:recipe_id])  # 修正：@recipe を先に定義する
+  @comment = @recipe.comments.find(params[:id])
+  
+  @comment.destroy
+  respond_to do |format|
+    flash.now[:notice] = 'コメントが削除されました'
+    format.js { render :index }
+  end
+end
 
   private
   # ストロングパラメーター

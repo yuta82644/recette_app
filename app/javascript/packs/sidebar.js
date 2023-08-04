b9be656eb217406f741d7ea9272a0ccb5126ff58
yuta <calendula6724yuta@gmail.com>
2023-08-04 14:01:16 +0900

    サイバー表示途中

--- app/javascript/packs/sidebar.js
+++ app/javascript/packs/sidebar.js
@@ -4,16 +4,19 @@ const nav = document.querySelector(".nav");
 
 toggler.addEventListener("click", () => {
   document.body.classList.toggle("show-nav");
+  toggler.classList.toggle("active"); // Add or remove the "active" class on toggle click
 });
 
 overlay.addEventListener("click", () => {
   document.body.classList.remove("show-nav");
+  toggler.classList.remove("active"); // Remove the "active" class when the overlay is clicked
 });
 
 // サイドバーのメニューをクリックしたら非表示
-const hrefLink = document.querySelectorAll('.linkList li a');
+const hrefLink = document.querySelectorAll('.nav ul li a');
 for (let i = 0; i < hrefLink.length; i++) {
   hrefLink[i].addEventListener("click", () => {
     document.body.classList.remove("show-nav");
+    toggler.classList.remove("active"); // Remove the "active" class when an <li> element is clicked
   });
 }

abba002c62ffcdc62ba95c4eed1c5959357ed291
yuta <calendula6724yuta@gmail.com>
2023-08-05 17:09:30 +0900

    タイトル検索フォーム完成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
 import "bootstrap";
 import "../stylesheets/application.scss";
+import "assets/stylesheets/application.scss";
 
 Rails.start()
 Turbolinks.start()

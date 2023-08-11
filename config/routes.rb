d4e3d33cbe45547b73dbabdb3c94efc296d5daea
yuta <calendula6724yuta@gmail.com>
2023-08-11 15:20:56 +0900

    ゲスト管理者　ゲストログイン削除できなくした

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,9 @@ Rails.application.routes.draw do
      resources :tasks
   end
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   devise_scope :user do
   post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'

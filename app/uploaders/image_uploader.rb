c6170d7bda0de334294556b51049e5ea67320e78
yuta <calendula6724yuta@gmail.com>
2023-08-15 18:20:24 +0900

    s3完了

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
+  if Rails.env.development? # 開発環境の場合
+  storage :file
+elsif Rails.env.test? # テスト環境の場合
+  storage :file
+else # 本番環境の場合
   storage :fog
+end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.

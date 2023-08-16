19f543f3926ff3a5fbaea2ac040650ca088df695
yuta <calendula6724yuta@gmail.com>
2023-08-16 15:06:46 +0900

    デプロイ修正

--- config/initializers/carriewave.rb
+++ config/initializers/carriewave.rb
@@ -1,31 +1,27 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory = 'recette-production'
-    config.fog_public = false
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1',
-      path_style: true
-    }
-  else
-    config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory = 'recette-development' # 開発環境用のバケット名
-    config.fog_public = false
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1',
-      path_style: true
-    }
+  require 'carrierwave/storage/abstract'
+  require 'carrierwave/storage/file'
+  require 'carrierwave/storage/fog'
+
+  config.storage :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region:                'ap-northeast-1',
+      path_style:            true,
+  }
+
+  config.fog_public     = false
+  config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
+
+  case Rails.env
+    when 'production'
+      config.fog_directory = 'recette-production'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/recette-production'
+    when 'development'
+      config.fog_directory = 'recette-production'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/recette-production'
   end
 end
